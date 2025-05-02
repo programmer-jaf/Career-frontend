a2fb0b9ac1392a5cb20a914eb06047fe8cf22364
Programmer-Jaf <programmerjaf@gmail.com>
2025-05-02 19:07:49 +0600

    implement routing and add Custom Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Route, Routes} from "react-router-dom";
+import PublicRoute from "./routes/PublicRoute.jsx";
+import {MainLayout} from "./layout/MainLayout.jsx";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+      {/*  Public Routes*/}
+        {
+          PublicRoute.map((route, index) => (
+              <Route path={route.path} key={index} element={route.element}/>
+          ))
+        }
+      {/*  Private Routes*/}
+        </Route>
+      </Routes>
   )
 }
 
