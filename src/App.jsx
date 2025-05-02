779a4b79308fa6bed10edcad85271901480bf8ed
Programmer-Jaf <programmerjaf@gmail.com>
2025-05-02 18:53:08 +0600

    install tailwindcss and some packages and fix some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
-  return <div>Hello world</div>;
-};
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      fontSize: {
+        h1: ["72px", { lineHeight: "82px" }],
+        h2: ["52px", { lineHeight: "62px" }],
+        h3: ["34px", { lineHeight: "44px" }],
+        "h3-sb": ["34px", { lineHeight: "44px", fontWeight: "700" }],
+        h4: ["26px", { lineHeight: "36px" }],
+        "h4-r": ["26px", { lineHeight: "40px", fontWeight: "700" }],
+        h5: ["24px", { lineHeight: "34px" }],
+        h6: ["22px", { lineHeight: "32px" }],
+        h7: ["20px", { lineHeight: "30px" }],
+        p1: ["18px", { lineHeight: "30px" }],
+        p2: ["16px", { lineHeight: "28px" }],
+        p3: ["14px", { lineHeight: "20px" }],
+        p4: ["12px", { lineHeight: "16px" }],
+        button1: ["18px", { lineHeight: "30px" }],
+        button2: ["16px", { lineHeight: "26px" }],
+        button3: ["14px", { lineHeight: "20px" }],
+      },
+      colors: {
+        "orange-primary": "#FF9966",
+        "pink-primary": "#EDA6E6",
+        "yellow-primary": "#FFF480",
+        "blue-primary": "#A8EBFF",
+        "green-primary": "#66CCA0",
+        white: "#FFFFFF",
+        "black-primary": "#110B08",
+        "grey-primary": "#574F4A",
+        light: "#F5F3EA",
+        "light-grey": "#E5E5EC",
+      },
+    },
+  },
+  plugins: [],
+};
