3a13db629da3ac07d528a08557e47f13711d8134
Programmer-Jaf <programmerjaf@gmail.com>
2025-05-04 13:03:03 +0600

    Add HeroSection component to BlogsPage for enhanced content display

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from '../sections/Blogs/HeroSection'
 
 const BlogsPage = () => {
   return (
     <div>
-      
+      <HeroSection/>
     </div>
   )
 }
---
+++ src/sections/Blogs/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center max-w-7xl px-4 py-16">
+      <h1 className="text-4xl md:text-h2 font-bold text-center text-black-primary max-w-[32rem] mb-4">
+        Top Career Advice, News & articles
+      </h1>
+      <p className="text-base md:text-p1 text-grey-primary text-center max-w-[28rem] mb-8 ">
+        Career Blog serves as a knowledge center for current trends in
+        contemporary work culture.
+      </p>
+    </div>
+  );
+};
+
+export default HeroSection;
